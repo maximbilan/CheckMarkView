81e710d2445651acba472cf0bff0c0b807071f6a
Maxim Bilan <maximb.mail@gmail.com>
2017-09-01 17:05:21 +0300

    Code review

--- CheckMarkView/CollectionViewController.swift
+++ CheckMarkView/CollectionViewController.swift
@@ -8,46 +8,39 @@
 
 import UIKit
 
-let reuseIdentifier = "Cell"
-
 class CollectionViewCell: UICollectionViewCell {
+
+	// MARK: - Constants
+	
+	static let reuseIdentifier = "Cell"
+	
+	// MARK: - Outlets
+	
 	@IBOutlet weak var imageView: UIImageView!
 	@IBOutlet weak var checkMarkView: CheckMarkView!
+	
 }
 
 class CollectionViewController: UICollectionViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: UICollectionViewDataSource
-
-	override func numberOfSections(in collectionView: UICollectionView) -> Int {
-		return 1
-	}
+    // MARK: - UICollectionViewDataSource
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 6
     }
 
 	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! CollectionViewCell
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.reuseIdentifier, for: indexPath as IndexPath) as! CollectionViewCell
 		
 		let imageName = String("image\(indexPath.row + 1).png")
 		cell.imageView.image = UIImage(named: imageName)
-		cell.checkMarkView.style = .GrayedOut
+		cell.checkMarkView.style = .grayedOut
 		cell.checkMarkView.setNeedsDisplay()
 		
 		return cell
 	}
 
-    // MARK: UICollectionViewDelegate
+    // MARK: - UICollectionViewDelegate
 
 	override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
 		let cell = collectionView.cellForItem(at: indexPath as IndexPath) as! CollectionViewCell
